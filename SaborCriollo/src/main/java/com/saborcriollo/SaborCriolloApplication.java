d44a56179c2edbf29e24bdf986656e0eb86ac15d
asoton <alfredosotonolazco@gmail.com>
2022-10-24 19:23:56 -0500

    Se agrega el proyecto

---
+++ SaborCriollo/src/main/java/com/saborcriollo/SaborCriolloApplication.java
@@ -0,0 +1,13 @@
+package com.saborcriollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborCriolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaborCriolloApplication.class, args);
+	}
+
+}
---
+++ SaborCriollo/src/test/java/com/saborcriollo/SaborCriolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saborcriollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaborCriolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
